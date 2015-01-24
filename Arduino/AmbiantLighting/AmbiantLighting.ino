6f85fc384471380fd04b5ff9f0e8efcfaffdb776
clement.kawczak <clement.kawczak@gmail.com>
2015-01-24 00:00:42 +0000

    [Arduino] Arduino can now read unique color for each led. It has to read stream color by group of 15 leds.

--- Arduino/AmbiantLighting/AmbiantLighting.ino
+++ Arduino/AmbiantLighting/AmbiantLighting.ino
@@ -4,42 +4,51 @@
 #define PIN            6
 
 // Number of leds
-#define LED_COUNT      30
+#define LEDS_COUNT      30
 
-Adafruit_NeoPixel pixels = Adafruit_NeoPixel(LED_COUNT, PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(LEDS_COUNT, PIN, NEO_GRB + NEO_KHZ800);
 
 //-----------------------------------------------------------
 void setup()
 {
 	// Init NeoPixel & Serial
 	pixels.begin();
+	pixels.show();
 	Serial.begin(57600);
 }
 
 //-----------------------------------------------------------
 void loop()
-{
-	while(Serial.available() >= 4)
+{     
+	// We have to read only 15 leds because serial can only have 64 bytes buffered
+	// We use different command (A & B) to divide data to read
+	// We only call pixels.show() at the end of the 'B' command reading to update all leds
+	while(Serial.available() >= 46)
 	{
-		// Read command
-		uint8_t dCmd		= Serial.read();
-		uint8_t dR			= Serial.read();
-		uint8_t dG			= Serial.read();
-		uint8_t dB			= Serial.read();
+		uint8_t dCmd = Serial.read();
 		switch (dCmd)
 		{
-			case 'C':
-			{
-				for(int nCpt = 0;nCpt < LED_COUNT;++nCpt)
+			case 'A':
+				for(int nCpt = 0;nCpt < 15;++nCpt)
 				{
+					uint8_t dR = Serial.read();
+					uint8_t dG = Serial.read();
+					uint8_t dB = Serial.read();
+					pixels.setPixelColor(nCpt, pixels.Color(dR, dG, dB));
+				}
+			break;
+			case 'B':
+				for(int nCpt = 15;nCpt < 30;++nCpt)
+				{
+					uint8_t dR = Serial.read();
+					uint8_t dG = Serial.read();
+					uint8_t dB = Serial.read();
 					pixels.setPixelColor(nCpt, pixels.Color(dR, dG, dB));
 				}
 				pixels.show();
-			}
 			break;
 			default:
 			break;
 		}
 	}
 }
-
